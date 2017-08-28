f7dd0dc4b7a2f98a1f59d42da8676092bdcd6aa6
shagunsinghal2207 <shagunsinghal2207@gmail.com>
2017-08-28 13:51:09 +0530

    service change

--- ml-search-server/api/controllers/searchController.js
+++ ml-search-server/api/controllers/searchController.js
@@ -6,23 +6,22 @@ var db = marklogic.createDatabaseClient(my.connInfo);
 var _ = require('lodash');
 
 exports.query = function(req, res) {
-    var qb = marklogic.queryBuilder;
     const pageNumber = req.query.page_no;
-    //const pageSize = 10;
     const pageSize = req.query.page_size;
     const start = (pageNumber - 1) * pageSize;
+
+    var qb = marklogic.queryBuilder;
     const query = db.documents.query(
         qb.where(qb.byExample({ tags: req.query.text }))
     )
     query.result(function(documents) {
-
         const end = start + pageSize;
-
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        // ToDo : this query needs to optimise to get the document of the current page only      
         const data = _.slice(documents, start, start + pageSize).map((document) => {
             return { url: document.uri, text: document.content.text.substring(0, 100) };
         })
-        res.send({ totalCounts: documents.length, data: data }); //this query needs to optimise to get the document of the current page only
+
+        res.send({ totalCounts: documents.length, data: data });
     }, function(error) {
         res.send(JSON.stringify(error, null, 2));
     });

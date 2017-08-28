c144579ba610da40e4aefb62911d8071deb83196
Devraj91 <devraj.roy91@gmail.com>
2017-08-28 15:13:58 +0530

    initial ui for search result (#1)
    * initial ui for search results

--- ml-search-web/src/app/app.module.ts
+++ ml-search-web/src/app/app.module.ts
@@ -4,21 +4,37 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import { searchReducer } from './store/reducers';
 import { SearchEffects } from './effects/SearchEffect';
+import { SearchContainerComponent } from './search-container/search-container.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { SearchResultComponent } from './search-result/search-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchContainerComponent,
+    SearchBoxComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     StoreModule.forRoot({search: searchReducer}),
-    EffectsModule.forRoot([SearchEffects])
+    EffectsModule.forRoot([SearchEffects]),
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: SearchContainerComponent
+      },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]

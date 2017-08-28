1dadcaa7f34b96990a0cf60d52d17c0bc30aedeb
shagunsinghal2207 <shagunsinghal2207@gmail.com>
2017-08-28 12:38:35 +0530

    changes in connection

--- ml-search-server/api/controllers/my-connection.js
+++ ml-search-server/api/controllers/my-connection.js
@@ -4,7 +4,7 @@ module.exports = {
         host: 'marklogicdb5672.cloudapp.net',
         port: 8000,
         user: 'sa',
-        password: 'Global@123',
+        password: 'Password', // Please enter database password...
         authType: 'DIGEST' // The default auth
     }
 };

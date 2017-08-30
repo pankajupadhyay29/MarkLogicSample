f57744ddf31544cc4238b64569a680922b4e8e72
devraj91 <devraj.roy91@gmail.com>
2017-08-30 19:15:17 +0530

    loading issue fixed

--- ml-search-web/src/app/search-box/search-box.component.ts
+++ ml-search-web/src/app/search-box/search-box.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class SearchBoxComponent implements OnInit {
   @Input() searchTerm: Array<any>;
+  @Input() loading: any;
   @Output() onSearch =  new EventEmitter();
   constructor() { }
 

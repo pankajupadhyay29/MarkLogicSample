054cf94c2da7bfd832eeca965b76b1a8734015f5
Pankaj Upadhyay <Pankaj.Upadhyay@synapse.com>
2017-08-30 07:57:08 +0530

    connection

---
+++ ml-search-server/my-connection.js
@@ -0,0 +1,10 @@
+module.exports = {
+    connInfo: {
+        database: 'Test',
+        host: 'MarkLogic server',
+        port: 8000,
+        user: 'User name',
+        password: 'password', // Please enter database password...
+        authType: 'DIGEST' // The default auth
+    }
+};
